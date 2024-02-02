78d078a9727046fd4b69fc71b2a85817de9a8355
Ole Stavland <olestavland@Stavland-2.local>
2024-02-02 17:00:59 +0100

    Introducing the updated code from semesterProject demo

--- routes/usersRoute.mjs
+++ routes/usersRoute.mjs
@@ -1,13 +1,22 @@
 import express, { response } from "express";
 import User from "../modules/user.mjs";
-import HttpCodes from "../modules/httpErrorCodes.mjs";
+import { HTTPCodes } from "../modules/httpConstants.mjs";
+import SuperLogger from "../modules/SuperLogger.mjs";
+
+
 
 
 const USER_API = express.Router();
+USER_API.use(express.json()); // This makes it so that express parses all incoming payloads as JSON for this route.
 
 const users = [];
 
-USER_API.get('/:id', (req, res) => {
+USER_API.get('/', (req, res, next) => {
+    SuperLogger.log("Demo of logging tool");
+    SuperLogger.log("An important msg", SuperLogger.LOGGING_LEVELS.CRTICAL);
+})
+
+USER_API.get('/:id', (req, res, next) => {
 
     // Tip: All the information you need to get the id part of the request can be found in the documentation 
     // https://expressjs.com/en/guide/routing.html (Route parameters)
@@ -24,7 +33,7 @@ USER_API.post('/', (req, res, next) => {
     const { name, email, password } = req.body;
 
     if (name != "" && email != "" && password != "") {
-        const user = new User();
+        const user = new user();
         user.name = name;
         user.email = email;
 
@@ -36,13 +45,13 @@ USER_API.post('/', (req, res, next) => {
 
         if (!exists) {
             users.push(user);
-            res.status(HttpCodes.SuccesfullRespons.Ok).end();
+            res.status(HTTPCodes.SuccesfullRespons.Ok).end();
         } else {
-            res.status(HttpCodes.ClientSideErrorRespons.BadRequest).end();
+            res.status(HTTPCodes.ClientSideErrorRespons.BadRequest).end();
         }
 
     } else {
-        res.status(HttpCodes.ClientSideErrorRespons.BadRequest).send("Mangler data felt").end();
+        res.status(HTTPCodes.ClientSideErrorRespons.BadRequest).send("Mangler data felt").end();
     }
 
 });
