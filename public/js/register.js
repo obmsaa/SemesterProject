67ed6c1179a19d00eb9f4cd198e63954994acbff
Ole Stavland <olestavland@Stavland-2.uia.no>
2024-02-22 21:27:39 +0100

    Fixed the eventlistener to properly send data and added logging where needed

--- public/js/register.js
+++ public/js/register.js
@@ -1,33 +1,38 @@
 
-    console.log('Javascript jogging...');
-
-
 
 //Variables
 const regForm = document.getElementById("regForm");
 
+  
 //Eventlistener for registering a user
 regForm.addEventListener("submit", async function (event) {
     event.preventDefault();
 
+   
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
-    const pswHash = document.getElementById("pswHash").value;
+    const password = document.getElementById("password").value;
+    const role = "user";
 
-    const user = { name, email, pswHash };
+    const user = { name, email, password, role };
 
-
+  
     try {
         const response = await postTo("/user", user);
-        const data = await response.json();
-
         if (response.ok) {
+            const data = await response.json();
             console.log("Registration successful", data);
+            regForm.textContent = "SUCCESFULL REGISTRATION, GO LOG IN!";
+      
         } else {
-            console.log("Registration failed:", data);
+            console.log("Registration failed:", response.statusText);
+
         }
     } catch (error) {
-        console.error("An error occurred:", error);
+        console.error(error);
+
+        throw new Error ("An error occurred:", error);
+        
     }
 });
 
@@ -35,16 +40,18 @@ regForm.addEventListener("submit", async function (event) {
   
 
 async function postTo(url, data) {
-    const header = {
+
+    let cfg = {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
+        },  body: JSON.stringify(data),
+        
     };
-
-
-    const response = await fetch(url, header);
+    console.log('Making request to:', url, 'with config:', cfg);
+    console.log('Data being sent:', JSON.stringify(data));
+    const response = await fetch(url, cfg);
     return response;
+
 }
 
