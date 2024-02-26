264b438590c40e47e2abeaeaf1af7774d35ec66c
Ole Stavland <olestavland@Stavland-2.uia.no>
2024-02-26 15:00:47 +0100

    Added a recipeRou te for the various API calls that need to be made later on

---
+++ routes/recipeRoute.mjs
@@ -0,0 +1,48 @@
+import express, { json } from "express";
+import Recipe from "../modules/recipe.mjs";
+import { HTTPCodes } from "../modules/httpConstants.mjs";
+import SuperLogger from "../modules/SuperLogger.mjs";
+import DBManager from "../modules/storageManager.mjs";
+
+
+
+
+const RECIPE_API = express.Router();
+RECIPE_API.use(express.json()); // This makes it so that express parses all incoming payloads as JSON for this route.
+
+
+// Get all recipes
+RECIPE_API.get('/', async (req, res, next) => {
+
+  try {
+    const recipes = await DBManager.findRecipes();
+    res.status(HTTPCodes.SuccesfullRespons.Ok).json(recipes);
+  } catch (error) {
+    res.status(HTTPCodes.ServerErrorRespons.InternalError).send('Could not fetch recipes.');
+  }
+});
+
+
+
+RECIPE_API.post('/', async (req, res, next) => { 
+
+  
+
+});
+
+
+RECIPE_API.put('/:id', (req, res) => {
+  /// TODO: Edit recipe
+  const recipe = new Recipe(); //TODO: The recipe info comes as part of the request 
+  recipe.save();
+})
+
+RECIPE_API.delete('/:id', (req, res) => {
+  /// TODO: Delete recipe.
+  const recipe = new Recipe(); //TODO: Actual recipe
+  recipe.delete();
+})
+
+export default RECIPE_API
+
+
