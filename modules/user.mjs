a27c08911fc0636b2897a32d2e29a1435a67f4c7
Ole Stavland <olestavland@Stavland-2.uia.no>
2024-02-26 15:04:09 +0100

    Postiioned the footer at the bottom

--- modules/user.mjs
+++ modules/user.mjs
@@ -20,7 +20,7 @@ class User {
 
     /// TODO: What happens if the DBManager fails to complete its task?
 
-    // We know that if a user object dos not have the ID, then it cant be in the DB.
+    // We know that if a user object does not have the ID, then it cant be in the DB.
     if (this.id == null) {
 
       return await DBManager.createUser(this) ;
