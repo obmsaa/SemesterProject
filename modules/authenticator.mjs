9c30370b6624d865380640f7b756b5c6ef92a979
Ole Stavland <olestavland@Stavland-2.uia.no>
2024-03-12 13:00:47 +0100

    Changed the returning of token to properly send token

--- modules/authenticator.mjs
+++ modules/authenticator.mjs
@@ -3,14 +3,13 @@ import jwt from "jsonwebtoken";
 
 
 export function giveToken(userId){
-
+    console.log("user id in token: ", userId)
     try{
     const secret = process.env.ACCESS_TOKEN_SECRET;
     const token = jwt.sign({ userId }, secret, { expiresIn: '2h' });
-    return {authenticated: true, token};
+    return token;
     } catch (error){
-        console.log('Error in signing a token', error);
-        return {authenticated: false};
+        throw new Error('Failed to generate token:', error);
     }
 }
 
@@ -22,12 +21,12 @@ export function verifyToken(token){
 
         const decoded = jwt.verify(token, secret); 
         const userId = decoded.userId; // Access the user ID from the decoded payload
-        return {authenticated: true, userId};
+        return {userAuthenticated: true, userId};
 
     } catch (error) {
         // Handle errors, such as invalid token or signature
         console.log('Error decoding JWT:', error);
-        return {authenticated: false};
+        return {userAuthenticated: false};
 
     }
 }
